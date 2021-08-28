3998a29cc6f59ce6285f7b601eda32d6169d1b55
hartie95 <mail@hartie95.de>
2021-08-28 16:20:58 +0200

    update for newer libnx version

--- source/payload.cpp
+++ source/payload.cpp
@@ -11,7 +11,7 @@ using namespace tsl::elm;
  tsl::elm::Element* Payload::toListItem(PayloadHandler* payloadHandler){
     auto item = new ListItem(this->name);
     std::function<bool(u64)> onClick = [item, payloadHandler, this](u64 keys) { 
-        if(keys & KEY_A){
+        if(keys & HidNpadButton_A){
             item->setText("rebooting");
             payloadHandler->rebootToPayload(this);
             return true; 
